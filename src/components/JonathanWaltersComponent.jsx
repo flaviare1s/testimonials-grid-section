52b258d9da46bf7dae68f7c4343eabff2d948538
Flávia Reis <flaviarster@gmail.com>
2024-03-30 15:30:32 -0300

    I started to create the components

---
+++ src/components/JonathanWaltersComponent.jsx
@@ -0,0 +1,19 @@
+import Jonathan from '../assets/image-jonathan.jpg'
+
+export const JonathanWaltersComponent = () => {
+  return (
+    <div>
+        <div>
+            <img src={ Jonathan } alt="Jonathan's photo" />
+            Jonathan Walters
+            Verified Graduate
+        </div>
+        <div>
+            The team was very supportive and kept me motivated
+        </div>
+        <div>
+            “ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I’ve made in myself. ”
+        </div>
+    </div>
+  )
+}
