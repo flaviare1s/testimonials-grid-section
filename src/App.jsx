d1dd95cb36404c528c5a75458e49ed184a9bcc07
Flávia Reis <flaviarster@gmail.com>
2024-03-30 16:05:13 -0300

    Adjusting the grid

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,28 @@ import { DanielCliffordComponent } from './components/DanielCliffordComponent'
 import { FooterComponent } from './components/FooterComponent'
 import { JeanetteHarmonComponent } from './components/JeanetteHarmonComponent'
 import { JonathanWaltersComponent } from './components/JonathanWaltersComponent'
+import { KiraWhittleComponent } from './components/KiraWhittleComponent'
 import { PatrickAbramsComponent } from './components/PatrickAbramsComponent'
 
 export function App() {
  
   return (
     <div className='font-Barlow bg-blue-50 px-6 py-[71px] lg:px-[165px] flex flex-col justify-center items-center m-auto lg:h-screen'>
-      <div className='grid col-span-1 lg:col-span-4'>
-        <div>
+      <div className='grid grid-cols-1 lg:grid-cols-4 lg:grid-rows-2'>
+        <div className='col-span-1 lg:col-span-2 lg:row-span-1'>
           <DanielCliffordComponent />
         </div>
-        <div>
+        <div className='col-span-1 lg:col-span-1 lg:row-span-1'>
           <JonathanWaltersComponent />
         </div>
-        <div>
+        <div className='col-span-1 lg:col-span-1 lg:row-span-2 order-last lg:order-none'>
+          <KiraWhittleComponent />
+        </div>
+        <div className='col-span-1 lg:col-span-1 lg:row-span-1'>
           <JeanetteHarmonComponent />
         </div>
-        <div>
+        <div className='col-span-1 lg:col-span-2 lg:row-span-1'>
           <PatrickAbramsComponent />
-        </div>
-        <div>
-
         </div>
       </div>
       <div>
--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,8 +1,8 @@
 export const FooterComponent = () => {
   return (
-    <div>
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="https://github.com/flaviare1s">Flávia Reis</a>.
+    <div className="mt-6 text-center text-xs">
+        Challenge by <a className="font-bold text-moderate_violet" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a className="font-bold text-moderate_violet" href="https://github.com/flaviare1s">Flávia Reis</a>.
     </div>
   )
 }
